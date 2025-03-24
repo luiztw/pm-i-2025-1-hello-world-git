e0930604c25c9914e50391dccfd0e6ac08322eff
Luiz Felipe <lipetw66@gmail.com>
2025-03-24 20:18:17 -0300

    Adicionado validação do Enter e Esc, e botão de limpar texto

--- hello-world.js
+++ hello-world.js
@@ -4,9 +4,18 @@ const messageP = document.getElementById('message');
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
+    } else if (event.key === 'Enter'){
+        helloWorld();
+    } else if (event.key === 'Escape'){
+        nameInput.value = '';
     }
 }
 
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
+}
+
+const limpartexto = () => {
+    nameInput.value = '';
+    messageP.innerText = '';
 }
